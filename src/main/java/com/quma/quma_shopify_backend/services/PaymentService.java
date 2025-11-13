3265e9acacf0d7b6efd11ccbf89b6b4783598a02
Lakshay Agarwal <lakshay452000@gmail.com>
2025-11-13 22:44:03 +0530

    added no authentication for health , home

--- src/main/java/com/quma/quma_shopify_backend/services/PaymentService.java
+++ src/main/java/com/quma/quma_shopify_backend/services/PaymentService.java
@@ -31,7 +31,7 @@ public class PaymentService {
 
     public InitiatePaymentResponseDTO startPayment(String orderId) {
         try {
-            Order entity = orderRepository.findById(orderId)
+            Order entity = orderRepository.findByOrderId(orderId)
                     .orElseThrow(() -> new ApiException("Order not found", 404));
 
             String receipt = entity.getOrderId();
@@ -55,7 +55,7 @@ public class PaymentService {
 
             return response;
         } catch (Exception e) {
-            log.error("Error while starting payment", e);
+            log.error("Error while starting payment", e.getMessage());
             throw new ApiException(e.getMessage(), 500);
         }
     }
