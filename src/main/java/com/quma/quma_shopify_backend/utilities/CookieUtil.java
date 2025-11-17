a31cdb20e901043d9b24abb3fd43ed2cdd3aef93
Lakshay452000 <67015856+Lakshay452000@users.noreply.github.com>
2025-11-17 12:04:34 +0530

    Removing domain from cookie

--- src/main/java/com/quma/quma_shopify_backend/utilities/CookieUtil.java
+++ src/main/java/com/quma/quma_shopify_backend/utilities/CookieUtil.java
@@ -13,7 +13,6 @@ public class CookieUtil {
                 .sameSite("None") // required for cross-origin cookies
                 .path(path)
                 .maxAge(maxAge)
-                .domain("quma-verse.vercel.app")
                 .build();
 
         response.addHeader("Set-Cookie", cookie.toString());
